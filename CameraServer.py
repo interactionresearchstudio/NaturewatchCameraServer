bb2e7c784167c9f2dabf0776f811d3b7d8f86c45
Mike Vanis <hello@mikevanis.com>
2017-06-01 16:53:46 +0100

    fixed tabs

--- CameraServer.py
+++ CameraServer.py
@@ -2,52 +2,56 @@ import cv2
 from imutils.video.pivideostream import PiVideoStream
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from SocketServer import ThreadingMixIn
 import time
 
 vs = PiVideoStream().start()
 time.sleep(2.0)
 
+
 class CamHandler(BaseHTTPRequestHandler):
-	def do_GET(self):
-		print self.path
-		if self.path.endswith('.mjpg'):
-			self.send_response(200)
-			self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
-			self.end_headers()
-			print("Serving mjpg...")
+    def do_GET(self):
+        print self.path
+        if self.path.endswith('.mjpg'):
+            self.send_response(200)
+            self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
+            self.end_headers()
+            print("Serving mjpg...")
             while True:
                 img = vs.read()
-                r, buf = cv2.imencode(".jpg",img)
-				self.wfile.write("--jpgboundary\r\n")
-				self.send_header('Content-type','image/jpeg')
-				self.send_header('Content-length',str(len(buf)))
-				self.end_headers()
-				self.wfile.write(bytearray(buf))
-				self.wfile.write('\r\n')
+                r, buf = cv2.imencode(".jpg", img)
+                self.wfile.write("--jpgboundary\r\n")
+                self.send_header('Content-type', 'image/jpeg')
+                self.send_header('Content-length', str(len(buf)))
+                self.end_headers()
+                self.wfile.write(bytearray(buf))
+                self.wfile.write('\r\n')
 
-		if self.path.endswith('.html') or self.path=="/":
-			self.send_response(200)
-			self.send_header('Content-type','text/html')
-			self.end_headers()
-			self.wfile.write('<html><head></head><body>')
-			self.wfile.write('<img src="http://naturewatch-cam.local:9090/cam.mjpg"/>')
-			self.wfile.write('</body></html>')
-			return
+        if self.path.endswith('.html') or self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write('<html><head></head><body>')
+            self.wfile.write('<img src="http://naturewatch-cam.local:9090/cam.mjpg"/>')
+            self.wfile.write('</body></html>')
+            return
+
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     """Handle requests in separate thread"""
 
+
 def main():
-	try:
-		server = ThreadedHTTPServer(('',9090),CamHandler)
-		print "server started"
-		server.serve_forever()
-	except KeyboardInterrupt:
-		server.socket.close()
+    try:
+        server = ThreadedHTTPServer(('', 9090), CamHandler)
+        print "server started"
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.socket.close()
+
 
 if __name__ == '__main__':
-	main()
+    main()
 
 vs.stop()
