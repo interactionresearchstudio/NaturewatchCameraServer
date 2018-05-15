c403b3597a7c39b1746775ff5be0991f4c89a3fc
Mike Vanis <hello@mikevanis.com>
2018-05-15 17:19:55 +0100

    hotfix for CORS issues with POST request

--- NaturewatchCameraServer.py
+++ NaturewatchCameraServer.py
@@ -19,6 +19,14 @@ isTimeSet = False
 
 # Handle HTTP requests.
 class CamHandler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
     def do_GET(self):
         print(self.path)
         if self.path.endswith('.mjpg'):
