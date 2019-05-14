0439764e15c4a0da9436e7df7e17c46c2e29454d
Mike Vanis <hello@mikevanis.com>
2019-05-14 09:53:02 +0100

    fixed .gitignore and commited files

--- naturewatchenv/lib/python3.7/site-packages/imutils/io/__init__.py
+++
@@ -1,2 +0,0 @@
-# import the necessary packages
-from .tempfile import TempFile
--- naturewatchenv/lib/python3.7/site-packages/imutils/video/__init__.py
+++
@@ -1,6 +0,0 @@
-# import the necessary packages
-from .count_frames import count_frames
-from .fps import FPS
-from .videostream import VideoStream
-from .webcamvideostream import WebcamVideoStream
-from .filevideostream import FileVideoStream
