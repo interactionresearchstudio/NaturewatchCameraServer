a8543b0a2c4d9dc84371b8dcdd870dce547aecd1
Mike Vanis <hello@mikevanis.com>
2019-06-04 17:39:54 +0100

    start cameracontroller thread and cleanup before and after file saver tests

--- tests/test_file_saver.py
+++ tests/test_file_saver.py
@@ -2,6 +2,7 @@ import pytest
 import sys
 import json
 import os
+import time
 sys.path.insert(0, './server')
 from FileSaver import FileSaver
 import naturewatch_camera_server
@@ -10,9 +11,13 @@ app = naturewatch_camera_server.create_app()
 file_saver = FileSaver()
 
 
-@pytest.fixture()
-def test_client():
+@pytest.fixture(autouse=True)
+def run_around_tests():
     testing_client = app.test_client()
+
+    while app.camera_controller.is_alive() is False:
+        app.camera_controller.start()
+        time.sleep(1)
 
     # Establish application context
     ctx = app.app_context()
@@ -28,4 +33,5 @@ def test_client():
 def test_image_save():
     filename = file_saver.save_image(app.camera_controller.get_image())
     assert os.path.isfile(filename)
-    
+    assert os.path.getsize(filename) != 0
+    os.remove(filename)
