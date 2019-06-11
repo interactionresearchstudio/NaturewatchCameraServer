f6353dcd5758a23af160768714a6b65d136dcc17
Mike Vanis <hello@mikevanis.com>
2019-06-11 15:50:45 +0100

    simplify ChangeDetector update loop

--- naturewatch_camera_server/ChangeDetector.py
+++ naturewatch_camera_server/ChangeDetector.py
@@ -5,6 +5,7 @@ import datetime
 import time
 import imutils
 import logging
+from naturewatch_camera_server.FileSaver import FileSaver
 
 
 class ChangeDetector(Thread):
@@ -19,6 +20,8 @@ class ChangeDetector(Thread):
         self.camera_controller = camera_controller
 
         self.logger = logger
+
+        self.file_saver = FileSaver()
 
         self.minWidth = self.config["min_width"]
         self.maxWidth = self.config["max_width"]
@@ -116,28 +119,6 @@ class ChangeDetector(Thread):
 
         return False
     
-    def capture_photo(self):
-        self.logger.info('Capturing a photo ...')
-        try:
-            self.hiResCapture.truncate(0)
-            self.hiResCapture.seek(0)
-            hrs = self.hiResStream.__next__()
-        except Exception as e:
-            self.logger.error('capture_photo() error: ')
-            self.logger.exception(e)
-            pass
-        else:
-            self.logger.debug('Captured %dx%d photo', self.hiResCapture.array.shape[1],
-                              self.hiResCapture.array.shape[0])
-        if self.config["rotate_camera"] is 1:
-            hi_res_image = imutils.rotate(hrs.array, angle=180)
-        else:
-            hi_res_image = hrs.array
-        saving_thread = Thread(target=self.save_photo, args=[hi_res_image])
-        saving_thread.start()
-        self.numOfPhotos = self.numOfPhotos + 1
-        self.lastPhotoTime = time.time()
-
     @staticmethod
     def get_largest_contour(contours):
         """
@@ -152,67 +133,11 @@ class ChangeDetector(Thread):
             maxIndex = np.argmax(areas)
             return contours[maxIndex]
 
-    def display_min_max(self, img):
-        if self.isMinActive is True:
-            minColour = self.activeColour
-            maxColour = self.inactiveColour
-        else:
-            minColour = self.inactiveColour
-            maxColour = self.activeColour
-
-        cv2.rectangle(img, (int(self.config["cv_width"]/2-self.minWidth/2),
-                            int(self.config["cv_height"]/2-self.minHeight/2)),
-                      (int(self.config["cv_width"]/2+self.minWidth/2),
-                       int(self.config["cv_height"]/2+self.minHeight/2)), minColour, 2)
-        cv2.rectangle(img, (int(self.config["cv_width"]/2-self.maxWidth/2),
-                            int(self.config["cv_height"]/2-self.maxHeight/2)),
-                      (int(self.config["cv_width"]/2+self.maxWidth/2),
-                       int(self.config["cv_height"]/2+self.maxHeight/2)), maxColour, 2)
-        return img
-
-    def set_sensitivity(self, min, max):
-        self.minWidth = min
-        self.minHeight = min
-        self.maxWidth = max
-        self.maxHeight = max
-
-    def increase_min_max(self, increment):
-        if self.isMinActive is True:
-            self.minWidth = self.minWidth + increment
-            self.minHeight = self.minHeight + increment
-            if self.minWidth > self.maxWidth:
-                self.minWidth = self.maxWidth
-                self.minHeight = self.maxHeight
-        else:
-            self.maxWidth = self.maxWidth + increment
-            self.maxHeight = self.maxHeight + increment
-            if self.maxWidth > self.config["cv_width"]:
-                self.maxWidth = self.config["cv_width"]
-                self.maxHeight = self.config["cv_width"]
-            if self.maxHeight >= self.config["cv_height"]:
-                self.maxHeight = self.config["cv_height"]
-
-    def decrease_min_max(self, increment):
-        if self.isMinActive is True:
-            self.minWidth = self.minWidth - increment
-            self.minHeight = self.minHeight - increment
-            if self.minWidth < 0:
-                self.minWidth = 0
-                self.minHeight = 0
-        else:
-            self.maxWidth = self.maxWidth - increment
-            self.maxHeight = self.maxHeight - increment
-            if self.maxWidth < self.minWidth:
-                self.maxWidth = self.minWidth
-                self.maxHeight = self.minHeight
-            if self.maxWidth < self.config["cv_height"]:
-                self.maxHeight = self.maxWidth
-            elif self.maxWidth >= self.config["cv_height"]:
-                self.maxHeight = self.config["cv_height"]
-
-    def arm(self):
-        self.logger.info('Starting photo capturing')
-        self.mode = "photo"
+    def set_sensitivity(self, min_width, max_width):
+        self.minWidth = min_width
+        self.minHeight = min_width
+        self.maxWidth = max_width
+        self.maxHeight = max_width
 
     def start_photo_session(self):
         self.logger.info('Starting photo capturing')
@@ -225,30 +150,10 @@ class ChangeDetector(Thread):
         self.session_start_time = time.time()
 
     def update(self):
-        try:
-            self.lowResCapture.truncate(0)
-            self.lowResCapture.seek(0)
-
-            lrs = self.lowResStream.__next__()
-
-            if self.config["rotate_camera"] is 1:
-                self.currentImage = imutils.rotate(lrs.array, angle=180)
-            else:
-                self.currentImage = lrs.array
-
-            if self.mode == "inactive":
-                self.currentImage = self.display_min_max(self.currentImage)
-            elif self.mode == "photo":
-                self.currentImage = self.detect_change_contours(self.currentImage)
-
-        except Exception as e:
-            self.logger.warning('update error')
-            self.logger.exception(e)
-            self.initialise_camera()
-            pass
-
-    def get_current_image(self):
-        return self.currentImage.copy()
+        if self.mode == "photo":
+            if self.detect_change_contours(self.camera_controller.get_image()):
+                self.logger.info("ChangeDetector: Detected motion. Taking photo...")
+                self.file_saver.save_image(self.camera_controller.get_splitter_image())
 
     @staticmethod
     def safe_width(width):
@@ -265,4 +170,3 @@ class ChangeDetector(Thread):
             return height
         else:
             return ChangeDetector.safe_height(height-1)
-
