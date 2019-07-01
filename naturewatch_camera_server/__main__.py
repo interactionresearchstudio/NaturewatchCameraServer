6f1786d2fe7a58df633829bc1ccfb410c234c2ec
Mike Vanis <hello@mikevanis.com>
2019-07-01 17:25:04 +0100

    run app without debugger

--- naturewatch_camera_server/__main__.py
+++ naturewatch_camera_server/__main__.py
@@ -3,4 +3,4 @@ from naturewatch_camera_server import create_app
 if __name__ == '__main__':
     app = create_app()
     app.camera_controller.start()
-    app.run(debug=True, threaded=True)
+    app.run(debug=False, threaded=True)
