8fb041b97576d29033382bac8f9742711e1c10f0
Mike Vanis <hello@mikevanis.com>
2019-07-01 17:45:41 +0100

    more explicit with port and host

--- naturewatch_camera_server/__main__.py
+++ naturewatch_camera_server/__main__.py
@@ -3,4 +3,4 @@ from naturewatch_camera_server import create_app
 if __name__ == '__main__':
     app = create_app()
     app.camera_controller.start()
-    app.run(debug=False, threaded=True)
+    app.run(debug=False, threaded=True, port=5000, host='0.0.0.0')
