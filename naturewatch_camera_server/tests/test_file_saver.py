e03b3ca06c45418f5f03eec3e77f2d81a25461f9
Mike Vanis <hello@mikevanis.com>
2020-01-30 16:23:35 +0000

    check storage test

--- naturewatch_camera_server/tests/test_file_saver.py
+++ naturewatch_camera_server/tests/test_file_saver.py
@@ -35,8 +35,22 @@ def run_around_tests():
 
 
 def test_image_save():
+    """
+    GIVEN a FileSaver instance
+    WHEN an image is saved
+    THEN the image should exist in the file system and should not be empty
+    """
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     filename = file_saver.save_image(app.camera_controller.get_md_image(), timestamp)
     assert os.path.isfile(app.user_config["photos_path"] + filename)
     assert os.path.getsize(app.user_config["photos_path"] + filename) != 0
     os.remove(app.user_config["photos_path"] + filename)
+
+
+def test_check_storage():
+    """
+    GIVEN a FileSaver instance
+    WHEN checkStorage is called
+    THEN percentage of available storage should be returned
+    """
+    assert file_saver.checkStorage() <= 100
