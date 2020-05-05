9e1c8ddbb30cb6ce1b8531f9b96ecce63aa1ce98
Mike Vanis <hello@mikevanis.com>
2020-05-05 15:56:57 +0100

    add build folder, add port option to __main__.py of module

--- naturewatch_camera_server/__main__.py
+++ naturewatch_camera_server/__main__.py
@@ -1,7 +1,13 @@
 from naturewatch_camera_server import create_app
+import argparse
+
+parser = argparse.ArgumentParser(description='Launch My Naturewatch Camera')
+parser.add_argument('-p', action='store', dest='port', default=5000,
+                    help='Port number to attach to')
+args = parser.parse_args()
 
 if __name__ == '__main__':
     app = create_app()
     app.camera_controller.start()
     app.change_detector.start()
-    app.run(debug=False, threaded=True, port=5000, host='0.0.0.0')
+    app.run(debug=False, threaded=True, port=args.port, host='0.0.0.0')
