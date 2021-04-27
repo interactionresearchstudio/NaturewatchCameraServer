2f8bb37787ba4ae4ed2915a04d88625452ba8448
Enrico Gueli <enrico.gueli@gmail.com>
2021-04-27 15:37:48 +0100

    Suggest to check the camera connection instead of pointing at raspi-config

--- naturewatch_camera_server/__main__.py
+++ naturewatch_camera_server/__main__.py
@@ -1,5 +1,7 @@
 from naturewatch_camera_server import create_app, create_error_app
+from picamera.exc import PiCameraError
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description='Launch My Naturewatch Camera')
 parser.add_argument('-p', action='store', dest='port', default=5000,
@@ -10,7 +12,11 @@ class CameraNotFoundException(Exception):
     pass
 
 def is_camera_enabled():
-    return True
+    # inspired by https://stackoverflow.com/questions/58250817/raspberry-pi-camera-module-check-if-connected#comment102874971_58250817
+    vcgencmd_result = subprocess.run(['/opt/vc/bin/vcgencmd', 'get_camera'], stdout=subprocess.PIPE)
+    result_text = vcgencmd_result.stdout.decode('utf-8').strip()
+    properties = dict(pair.split('=') for pair in result_text.split(' '))
+    return properties['supported'] == '1'
 
 if __name__ == '__main__':
     try:
@@ -18,7 +24,7 @@ if __name__ == '__main__':
         app.camera_controller.start()
         app.change_detector.start()
     except Exception as e:
-        if e is CameraError and "Camera is not enabled".contains(e.message):
+        if isinstance(e, PiCameraError) and "Camera is not enabled" in str(e):
             # This error message appears even if the camera _is_ enabled, but the camera is not found.
             # e.g. due to a connection problem.
             # We don't want to mislead users into messing with raspi-config, so check if the
