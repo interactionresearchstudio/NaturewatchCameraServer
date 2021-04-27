3ff21ed9a6fddb9832392a2a42c1b0ce620886f5
Enrico Gueli <enrico.gueli@gmail.com>
2021-04-27 16:07:08 +0200

    WIP throw different exception is camera is enabled

--- naturewatch_camera_server/__main__.py
+++ naturewatch_camera_server/__main__.py
@@ -6,12 +6,26 @@ parser.add_argument('-p', action='store', dest='port', default=5000,
                     help='Port number to attach to')
 args = parser.parse_args()
 
+class CameraNotFoundException(Exception):
+    pass
+
+def is_camera_enabled():
+    return True
+
 if __name__ == '__main__':
     try:
         app = create_app()
         app.camera_controller.start()
         app.change_detector.start()
     except Exception as e:
+        if e is CameraError and "Camera is not enabled".contains(e.message):
+            # This error message appears even if the camera _is_ enabled, but the camera is not found.
+            # e.g. due to a connection problem.
+            # We don't want to mislead users into messing with raspi-config, so check if the
+            # camera interface is really disabled.
+            if (is_camera_enabled()):
+                e = CameraNotFoundException("Unable to access camera. Is the cable properly connected?")
+
         app = create_error_app(e)
 
     app.run(debug=False, threaded=True, port=args.port, host='0.0.0.0')
